1b0da7399d905832466ad3b765a31eeee8387d15
motoki kyan <kyan@ryukyuhub.co.jp>
2023-03-22 17:22:43 +0900

    トップページ修正した

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
 <div class="container">
 	<div class="row">
 		<div class="d-flex justify-content-center">
-			<button class="m-3 py-3 px-5 btn-dark"><a href="{{ route('password.index') }}" class="text-decoration-none text-light">検索</a></button>
+			<button class="m-3 py-3 px-5 btn-dark"><a href="{{ route('search') }}" class="text-decoration-none text-light">検索</a></button>
 			<button class="m-3 py-3 px-5 btn-dark"><a href="{{ route('password.create') }}" class="text-decoration-none text-light">生成</a></button>
 			<button class="m-3 py-3 px-5 btn-dark"><a href="{{ route('password.entry') }}" class="text-decoration-none text-light">登録</a></button>
 			<button class="m-3 py-3 px-5 btn-dark"><a href="{{ route('login') }}" class="text-decoration-none text-light">ログイン</a></button>
